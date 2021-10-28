a5c5f2d32c7689c1e0419ad103e3b763ccad9a83
imlyzh <enterindex@gmail.com>
2021-10-28 23:31:30 +0800

    modify call and lambda logic

--- src/partial_evaluation/call.rs
+++ src/partial_evaluation/call.rs
@@ -12,12 +12,19 @@ impl PartialCall for Lambda {
     fn partial_call(&self, env: Arc<NameSpace>, params: Pair<Params<Result<Value, Ast>>>) -> Option<Result<Value, Ast>> {
         let env = Arc::new(env.new_level());
         let params = self.0.partial_matchs(&params)?;
+        let partial_count = params
+            .iter()
+            .filter(|(_,y)| y.is_err()).count();
         for (k, v) in params {
             if let Ok(v) = v {
                 env.add(k, v);
             }
         }
-        Some(self.1.partial_eval(env))
+        if partial_count == 0 {
+            Some(self.1.partial_eval(env))
+        } else {
+            todo!()
+        }
     }
 }
 
